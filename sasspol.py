6af44b530fcd40d460db37fa124387e4cfd2259b
MenacingMecha <mackaycallum99@gmail.com>
2019-03-31 20:27:44 +0100

    Added debug test for Game class

--- sasspol.py
+++ sasspol.py
@@ -9,18 +9,29 @@ class Game:
         self.has_been_played = False
         self.last_played_timestamp = 0
 
-    def set_last_played_timestamp(date: str):
+    def set_last_played_timestamp(self, date: str):
         self.has_been_played = True
-        self.last_played_timestamp = (get_timestamp_from_date(date))
+        self.last_played_timestamp = (self.get_timestamp_from_date(date))
 
-    def get_timestamp_from_date(date:str) -> int:
+    @staticmethod
+    def get_timestamp_from_date(date: str) -> int:
         ''' Converts a date string into int unix epoch time.
         date argument must be formatted as dd/mm/yy
         '''
         return time.mktime(datetime.datetime.strptime(date, "%d/%m/%y").timetuple())
 
+def debug_test_game_class():
+    print("debug_test_game_class() START")
+    test_game = Game("mario brother", "jump")
+    print("name: " + test_game.name)
+    print("genre: " + test_game.genre)
+    last_date_played = "31/03/19"
+    test_game.set_last_played_timestamp(last_date_played)
+    print("last played timestamp: " + str(test_game.last_played_timestamp))
+    print("debug_test_game_class() END")
+
 def main():
-    pass
+    debug_test_game_class()
 
 if __name__ == "__main__":
     main()
